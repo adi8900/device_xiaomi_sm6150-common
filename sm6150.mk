790242fd4b2e5f4a874734cf461ca1b8e95a1aaf
Einar Gednochsson <pete80pro.pb@gmail.com>
2023-10-07 09:37:38 +0200

    sm6150-common: Define QCOM hardware for hals
    Signed-off-by: Einar Gednochsson <pete80pro.pb@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -26,6 +26,9 @@ PRODUCT_CHARACTERISTICS := nosdcard
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+PRODUCT_BOARD_PLATFORM := sm6150
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
