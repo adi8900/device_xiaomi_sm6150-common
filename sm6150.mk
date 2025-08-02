25eea65799379465237f85efc3310bd05716b76d
adi8900 <adrianszymanski242@gmail.com>
2025-08-02 23:36:00 +0200

    sm6150-common: Drop telephony packages
    * defined in yaap vendor
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -250,22 +250,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
-# Telephony
-PRODUCT_PACKAGES += \
-    extphonelib \
-    extphonelib-product \
-    extphonelib.xml \
-    extphonelib_product.xml \
-    qti-telephony-hidl-wrapper \
-    qti-telephony-hidl-wrapper-prd \
-    qti-telephony-utils \
-    qti_telephony_hidl_wrapper.xml \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
