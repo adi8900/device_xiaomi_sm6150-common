09d3ef17ffee4710b2e5caaa73dd2369e94b40dd
adi8900 <adrianszymanski242@gmail.com>
2025-08-02 23:24:06 +0200

    sm6150-common: Move board flag from boardconfig
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -35,6 +35,9 @@ PRODUCT_PACKAGES += \
     vendor_dsp_mountpoint \
     vendor_firmware_mnt_mountpoint
 
+# Platform
+TARGET_BOARD_PLATFORM := sm6150
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
