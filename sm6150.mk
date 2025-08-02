e2a71abf8b6b8043c3a3a19cc8c40f0d4cc0a081
adi8900 <adrianszymanski242@gmail.com>
2025-08-02 23:32:01 +0200

    sm6150-common: Drop lineage health
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -202,15 +202,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
